58a0c90f4fd443d22f866d3e86cde945cd777185
luismaldonado.dg <luismaldonado.dg@gmail.com>
2018-11-26 22:25:12 -0500

    cortes v1.8

---
+++ cortes/templates/page-component-banner.php
@@ -0,0 +1,26 @@
+<section class="content advanced-component banner">
+    <div class="main-slider slides owl-carousel">
+        <div class="item">
+            <div class="image">
+                <a>
+                    <picture>
+                        <source media="(max-width: 480px)" srcset="http://www.bmcbec.com.co/media/1509/foto_termocandelaria2.jpg?anchor=center&mode=crop&width=1600&height=550&rnd=131401768080000000">
+                        <img src="http://www.bmcbec.com.co/media/1509/foto_termocandelaria2.jpg?anchor=center&mode=crop&width=1600&height=550&rnd=131401768080000000" alt="alt">
+                    </picture>
+                </a>                                            
+            </div>
+            <div class="info">
+                <div class="container valign">
+                    <div class="row">
+                        <div class="col-md-6 col-xs-12 text-right pull-right">
+                            <p class="heading main">Principales informes del Sector de Gas en Colombia</p>
+                            <div class="text">
+                                <p>Conozca un resumen general del comportamiento transaccional y operativo del mercado de gas natural en Colombia</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
