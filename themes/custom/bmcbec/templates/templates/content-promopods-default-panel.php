3aff397f233e373015155ea07c31d04e4ef72fae
Valentina <valentina.aguirre@ariadnacg.com>
2019-04-02 09:20:00 -0500

    Se agregan templates

---
+++ themes/custom/bmcbec/templates/templates/content-promopods-default-panel.php
@@ -0,0 +1,27 @@
+<div class="item panel panel-default">
+	<div class="panel-body">
+		<p class="heading">Documentos de registro ante el Gestor</p>
+		<div class="info">
+			<div class="text">
+				<p><span class="intro"><a href="#" title="Vinculación">- Vinculación</a></span></p>
+				<p><span class="intro"><a href="#" title="Actualización">- Actualización</a></span></p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="item panel panel-default color-bg-main-blue">
+	<div class="panel-body">
+		<a href="#">
+			<p class="heading">Sitios de Interés </p>
+		</a>
+		<div class="info">
+			<div class="text">
+				<p>Conozca todas las entidades que hacen parte del sector del Gas Natural en Colombia.</p>
+			</div>
+			<p class="link">
+				<a href="#" class="btn btn-green">Ver todos</a>
+			</p>
+		</div>
+	</div>
+</div>
---
+++ themes/custom/bmcbec/templates/templates/content-promopods-list-panel.php
@@ -0,0 +1,51 @@
+<div class="item panel lists">
+    <div class="panel-body">
+        <div class="listing blog">
+            <p class="heading">Publicaciones Recientes</p>
+           
+            <div class="item">
+                <div class="info">
+                    <a href="#">
+                        <p class="heading">
+                            Gas natural para la competitividad industrial
+                        </p>
+                    </a>
+                </div>
+                <div class="meta">
+                    <div>
+                        <time><span class="date">23 Nov 2018</span></time>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="info">
+                    <a href="#">
+                        <p class="heading">
+                            G-070 B INF Información Subasta Úselo o Véndalo de Corto Plazo de Transporte
+                        </p>
+                    </a>
+                </div>
+                <div class="meta">
+                    <div>
+                        <time><span class="date">22 Nov 2018</span></time>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="info">
+                    <a href="#">
+                        <p class="heading">
+                            Ecopetrol adjudicó contrato para construcción de parque solar en Meta
+                        </p>
+                    </a>
+                </div>
+                <div class="meta">
+                    <div>
+                        <time><span class="date">22 Nov 2018</span></time>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="see_all" href="#">Ver todos</a>
+    </div>
+</div>
