e4a1779ad191ad58244fa73f5d4f5d259ed928fd
luismaldonado.dg <luis.maldonado@ariadnacg.com>
2019-04-10 05:55:41 -0500

    page gobierno corporativo v.1.0.0

---
+++ cortes/templates/page-component-image-caption.php
@@ -0,0 +1,6 @@
+<div class="page_component image">
+    <img src="http://www.bmcbec.com.co/media/1693/plantilla-banner-web-15.png" alt="">
+    <div class="info">
+      <div class="text">Gobierno Corporativo Bolsa Mercantil de Colombia</div>
+    </div>
+</div>
---
+++ cortes/templates/page-component-resources.php
@@ -0,0 +1,46 @@
+<div class="page_component resources">
+    <h3>Enlaces de interés sobre el Gobierno Corporativo de la Bolsa Mercantil de Colombia (BMC):</h3>
+
+    <div class="list-group">
+        <a href="http://www.bolsamercantil.com.co/GobiernoCorporativo.aspx" title="Link will open in a new window/tab" target="blank" class="item list-group-item odd">
+            <p class="heading primary_colour_1-text">Gobierno Corporativo<i class="fa fa-external-link pull-right after highlight primary_colour_1-text"></i></p>
+            <div class="size">Link externo - http://www.bolsamercantil.com.co/GobiernoCorporativo.aspx</div>
+        </a>
+        <a href="http://www.bolsamercantil.com.co/GobiernoCorporativo/Asamblea.aspx" title="Link will open in a new window/tab" target="blank" class="item list-group-item even">
+            <p class="heading primary_colour_1-text">Asamblea<i class="fa fa-external-link pull-right after highlight primary_colour_1-text"></i></p>
+            <div class="size">Link externo - http://www.bolsamercantil.com.co/GobiernoCorporativo/Asamblea.aspx</div>
+        </a>
+        <a href="http://www.bolsamercantil.com.co/GobiernoCorporativo/Asamblea.aspx" title="Link will open in a new window/tab" target="blank" class="item list-group-item odd">
+            <p class="heading primary_colour_1-text">Reglamento de la Asamblea<i class="fa fa-external-link pull-right after highlight primary_colour_1-text"></i></p>
+            <div class="size">Link externo - http://www.bolsamercantil.com.co/GobiernoCorporativo/Asamblea.aspx</div>
+        </a>
+        <a href="http://www.bolsamercantil.com.co/informacion/" title="Link will open in a new window/tab" target="blank" class="item list-group-item even">
+            <p class="heading primary_colour_1-text">Información de la Junta Directiva<i class="fa fa-external-link pull-right after highlight primary_colour_1-text"></i></p>
+            <div class="size">Link externo - http://www.bolsamercantil.com.co/informacion/</div>
+        </a>
+        <a href="http://www.bolsamercantil.com.co/GobiernoCorporativo/NuestroEquipo.aspx" title="Link will open in a new window/tab" target="blank" class="item list-group-item odd">
+            <p class="heading primary_colour_1-text">Reglamento de los Comites<i class="fa fa-external-link pull-right after highlight primary_colour_1-text"></i></p>
+            <div class="size">Link externo - http://www.bolsamercantil.com.co/GobiernoCorporativo/NuestroEquipo.aspx</div>
+        </a>
+        <a href="http://www.bolsamercantil.com.co/GobiernoCorporativo.aspx" title="Link will open in a new window/tab" target="blank" class="item list-group-item even">
+            <p class="heading primary_colour_1-text">Sistema de Administración de Riesgos<i class="fa fa-external-link pull-right after highlight primary_colour_1-text"></i></p>
+            <div class="size">Link externo - http://www.bolsamercantil.com.co/GobiernoCorporativo.aspx</div>
+        </a>
+        <a href="http://www.bolsamercantil.com.co/plan-de-mejoramiento/" title="Link will open in a new window/tab" target="blank" class="item list-group-item odd">
+            <p class="heading primary_colour_1-text">Plan de Mejoramiento<i class="fa fa-external-link pull-right after highlight primary_colour_1-text"></i></p>
+            <div class="size">Link externo - http://www.bolsamercantil.com.co/plan-de-mejoramiento/</div>
+        </a>
+        <a href="http://www.bolsamercantil.com.co/GobiernoCorporativo/NuestroEquipo.aspx" title="Link will open in a new window/tab" target="blank" class="item list-group-item even">
+            <p class="heading primary_colour_1-text">Composición de la Presidencia<i class="fa fa-external-link pull-right after highlight primary_colour_1-text"></i></p>
+            <div class="size">Link externo - http://www.bolsamercantil.com.co/GobiernoCorporativo/NuestroEquipo.aspx</div>
+        </a>
+        <a href="http://www.bolsamercantil.com.co/GobiernoCorporativo/Sociedad.aspx" title="Link will open in a new window/tab" target="blank" class="item list-group-item odd">
+            <p class="heading primary_colour_1-text">Documentos Corporativos<i class="fa fa-external-link pull-right after highlight primary_colour_1-text"></i></p>
+            <div class="size">Link externo - http://www.bolsamercantil.com.co/GobiernoCorporativo/Sociedad.aspx</div>
+        </a>
+        <a href="http://www.bolsamercantil.com.co/GobiernoCorporativo/Sociedad.aspx" title="Link will open in a new window/tab" target="blank" class="item list-group-item even">
+            <p class="heading primary_colour_1-text">Código País<i class="fa fa-external-link pull-right after highlight primary_colour_1-text"></i></p>
+            <div class="size">Link externo - http://www.bolsamercantil.com.co/GobiernoCorporativo/Sociedad.aspx</div>
+        </a>
+    </div>
+</div>
