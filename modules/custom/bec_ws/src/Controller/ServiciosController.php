2b23c6d692fb14b2b5d9fb55244a1e2878e20cf3
valen92 <valen92_@hotmail.com>
2019-04-22 20:51:38 -0500

    Se agrega nueva grafica

--- modules/custom/bec_ws/src/Controller/ServiciosController.php
+++ modules/custom/bec_ws/src/Controller/ServiciosController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ServiciosController extends ControllerBase
 {
-    
+
     public function sendGetRequest($url){
       try {
         $client = \Drupal::httpClient();
@@ -26,15 +26,15 @@ class ServiciosController extends ControllerBase
       try {
         $client = \Drupal::httpClient();
         $headers = array('Content-Type' => 'application/x-www-form-urlencoded');
-        $request = $client->post($url, array('headers' => $headers, 'body' => $param));        
-        $response = array('state' => 1,'response' =>json_decode($request->getBody()) );        
+        $request = $client->post($url, array('headers' => $headers, 'body' => $param));
+        $response = array('state' => 1,'response' =>json_decode($request->getBody()) );
       } catch (Exception $e) {
         $response = array('state' => 0,'response' =>$e->getMessage() );
       }
       return $response;
     }
 
-    public function enviarPeticionPost($metodo,$param,$formatear,$fechaCompleta,$filtro,$campos,$cantidad){      
+    public function enviarPeticionPost($metodo,$param,$formatear,$fechaCompleta,$filtro,$campos,$cantidad){
       $url=\Drupal::config('bec_ws.settings')->get('url_base').$metodo;
       $data=$this->sendPostRequest($url,$param);
       if ($formatear) {
@@ -62,25 +62,29 @@ class ServiciosController extends ControllerBase
           case 'getCantidadTomadaDiariamenteSnt':
             $data=$this->energiaSuministrar($data['response'],$fechaCompleta,$cantidad);
           break;
-          
+
+          case 'getCantidadContratadaPorPuntoDeEntrega':
+            $data=$this->energiaSuministrar($data['response'],$fechaCompleta,$cantidad);
+          break;
+
           default:
             # code...
             break;
         }
-        
+
       }
       return $data;
     }
 
     public function energiaInyectada($data,$fechaCompleta,$filtro,$cantidad){
       $info=array();
-      foreach ($data as $item) {          
-        if ($fechaCompleta) {                  
+      foreach ($data as $item) {
+        if ($fechaCompleta) {
           if (isset($info[$item->año][$item->mes][$item->dia][$item->$filtro])) {
             $info[$item->año][$item->mes][$item->dia][$item->$filtro]=$info[$item->año][$item->mes][$item->dia][$item->$filtro]+$item->$cantidad;
           }else{
-            $info[$item->año][$item->mes][$item->dia][$item->$filtro]=$item->$cantidad;            
-          }        
+            $info[$item->año][$item->mes][$item->dia][$item->$filtro]=$item->$cantidad;
+          }
           ksort($info[$item->año]);
         }
       }
@@ -93,10 +97,10 @@ class ServiciosController extends ControllerBase
               foreach ($value2 as $key3 => $value3) {
                 $dia=$key3;
                 array_push($labels, $dia.'/'.$mes.'/'.$año);
-                  foreach ($value3 as $key4 => $value4) {                     
+                  foreach ($value3 as $key4 => $value4) {
                     if (!is_array($format[$key4])) {
                       $format[$key4]=array();
-                    }                     
+                    }
                      array_push($format[$key4],$value4 );
                   }
               }
@@ -108,13 +112,13 @@ class ServiciosController extends ControllerBase
 
     public function energiaSuministrar($data,$fechaCompleta,$cantidad){
       $info=array();
-      foreach ($data as $item) {          
-        if ($fechaCompleta) {                  
+      foreach ($data as $item) {
+        if ($fechaCompleta) {
           if (isset($info[$item->año][$item->mes][$item->dia])) {
             $info[$item->año][$item->mes][$item->dia]=$info[$item->año][$item->mes][$item->dia]+$item->$cantidad;
           }else{
-            $info[$item->año][$item->mes][$item->dia]=$item->$cantidad;            
-          }        
+            $info[$item->año][$item->mes][$item->dia]=$item->$cantidad;
+          }
           ksort($info[$item->año]);
         }
       }
