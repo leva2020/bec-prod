ba32df04fdd8803eae86233d21068c6da8f29056
Eduardo Velasco <leva2020@gmail.com>
2019-06-25 21:10:31 -0500

    path publications filter

--- modules/custom/blocks_custom/src/Plugin/Block/CategoriesCustomBlock.php
+++ modules/custom/blocks_custom/src/Plugin/Block/CategoriesCustomBlock.php
@@ -21,41 +21,26 @@ class CategoriesCustomBlock extends BlockBase
      */
     public function build()
     {
-        $tree = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree(
-            'publicaciones', // This is your taxonomy term vocabulary (machine name).
-            0,                 // This is "tid" of parent. Set "0" to get all.
-            1,                 // Get only 1st level.
-            TRUE               // Get full load of taxonomy term entity.
-        );
-
-        $result = [];
-
-        foreach ($tree as $term) {
-            $result['tid'] = $term->tid;
-            $result['name'] = $term->name;
-        }
-
-        /*$vid = 'publicaciones';
-        //$terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vid);
-        $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->taxonomy_get_tree($vid);
+        $vid = 'publicaciones';
+        $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vid, 0);
 
         /*$tree = [];
         foreach ($terms as $tree_object) {
             $this->buildTree($tree, $tree_object, $vid);
         }*/
 
-        /*foreach ($terms as $term) {
+        foreach ($terms as $term) {
             $term_data[] = array(
                 "id" => $term->tid,
                 "name" => $term->name,
                 "weight" => $term->children,
                 "parent" => $term->parents,
             );
-        }*/
+        }
         return array(
             '#theme' => 'categories_block_custom',
             '#titulo' => $this->t('Listado Categorias'),
-            '#terms' => $result
+            '#terms' => $term_data
         );
     }
 
