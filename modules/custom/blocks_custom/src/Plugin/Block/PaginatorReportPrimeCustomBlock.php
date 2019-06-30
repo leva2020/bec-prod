cd8831190dae71631b22ed25e0b49d3a33ff5903
Eduardo Velasco <leva2020@gmail.com>
2019-06-30 09:48:00 -0500

    reports detail

---
+++ modules/custom/blocks_custom/src/Plugin/Block/PaginatorReportPrimeCustomBlock.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\blocks_custom\Plugin\Block;
+
+use Drupal;
+use Drupal\Core\Block\BlockBase;
+use Drupal\node\Entity\Node;
+
+/**
+ * Definición de nuestro bloque
+ *
+ * @Block(
+ *   id = "paginator_reports_prime_block",
+ *   admin_label = @Translation("Paginator Informes Prime")
+ * )
+ */
+class PaginatorReportPrimeCustomBlock extends BlockBase
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build()
+    {
+        $query = Drupal::entityQuery('node')
+            ->condition('type', 'informe_prime')
+            ->sort('created', 'DESC')
+            ->execute();
+        $reports = [];
+
+        $nid = 0;
+        $node = \Drupal::routeMatch()->getParameter('node');
+        if ($node instanceof \Drupal\node\NodeInterface) {
+            $nid = $node->id();
+        }
+
+        if (!empty($query)) {
+            $i = 0;
+            foreach ($query as $reportId) {
+                $report = Node::load($reportId);
+                if ($reportId != $nid && $i < 2) {
+                    $reports[] = $report;
+                    $i ++;
+                }
+            }
+        }
+
+        return array(
+            '#theme' => 'paginator_reports_prime_block_custom',
+            '#titulo' => $this->t('Paginator Listado Reportes'),
+            '#reports' => $reports
+        );
+    }
+
+}
