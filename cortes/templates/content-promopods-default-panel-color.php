b325e79163146310cba010e568bb15b44bf9ac55
Eduardo Velasco <evelasco@vi-datec.com.com>
2019-12-18 20:07:22 -0500

    enlaces, textos

--- cortes/templates/content-promopods-default-panel-color.php
+++ cortes/templates/content-promopods-default-panel-color.php
@@ -1,16 +1,19 @@
-<div class="item panel panel-default color-bg-green"><!--Estas clases definen el color de fondo "color-bg-green" "color-bg-main-blue" "color-bg-sec-blue"-->
-	<div class="panel-body">
-		<p class="heading">Nuestras Oficinas</p>
-		<div class="info">
-			<div class="text">
-				<p>
-					Calle 113 No. 7-21<br>
-					Piso 15 Torre A <br>
-					Edif. Teleport Business Park<br>
-					Pbx: +57(1) 629-2529<br>
-					Bogotá D.C., Colombia
-				</p>
-			</div>
-		</div>  
-	</div>
+<div class="item panel panel-default color-bg-green">
+    <!--Estas clases definen el color de fondo "color-bg-green" "color-bg-main-blue" "color-bg-sec-blue"-->
+    <div class="panel-body">
+        <p class="heading">Nuestras Oficinas</p>
+        <div class="info">
+            <div class="text">
+                <p>
+                    Calle 113 No. 7-21<br>
+                    Piso 15 Torre A <br>
+                    Edif. Teleport Business Park<br>
+                    Pbx: +57(1) 629-2529<br>
+                    Bogotá D.C., Colombia
+                </p>
+            </div>
+            <p class="link"><a href="/contáctenos/#pos_1327" class="btn secondary_colour_3-bg primary_colour_1-text">Escríbanos
+                    sus comentarios</a></p>
+        </div>
+    </div>
 </div>
