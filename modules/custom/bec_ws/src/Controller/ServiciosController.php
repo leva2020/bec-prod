575021c0cc5ecd855769b8b0a2f83cfa234ed120
Eduardo Velasco <evelasco@vi-datec.com.com>
2020-01-07 23:10:56 -0500

    ajuste grafica

--- modules/custom/bec_ws/src/Controller/ServiciosController.php
+++ modules/custom/bec_ws/src/Controller/ServiciosController.php
@@ -245,7 +245,7 @@ class ServiciosController extends ControllerBase
         if (is_array($data['response'])) {
             foreach ($data['response'] as $key => $value) {
                 $date = $value->fecha;
-                $date = date("Y/m", strtotime($date));
+                $date = date("Y/m/d", strtotime($date));
                 if (!in_array($date, $labels)):
                     $labels[] = $date;
                     $dataDataSets["capac_cant"][$date] = 0;
