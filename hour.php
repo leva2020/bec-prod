2a4b908f6274fc312b5a8da1e9d97974791486ac
Valentina <valena.92@gmail.com>
2020-02-03 23:34:37 +0000

    archivo hora

--- hour.php
+++ hour.php
@@ -1,3 +1,3 @@
 <?php
 
-echo '<span>' . date('l j, Y, g:i a') . '</span>';
+echo '<span style="color:#2e89b6;font-size:95%;ont-family: "Varela Round", "sans-serif" !important;font-weight: 700; ">' . date('l j, Y, g:i a') . '</span>';
