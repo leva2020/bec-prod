814a358ae78d365b57b3b05a613edc392b406e6d
Valentina <valena.92@gmail.com>
2020-02-04 00:01:15 +0000

    ajuste hora

--- hour.php
+++ hour.php
@@ -1,3 +1,8 @@
 <?php
 
-echo '<span style="color:#2e89b6;font-size:95%;ont-family: "Varela Round", "sans-serif" !important;font-weight: 700; ">' . date('l j, Y, g:i a') . '</span>';
+setlocale(LC_ALL, "es_CO");
+
+date_default_timezone_set('America/Bogota');
+
+echo '<span style="color:#2e89b6;font-size:95%;font-family: Varela Round, sans-serif;font-weight: 700; ">' .  strftime("%A %d/%b/%Y %R", mktime()) . '</span>';
+
