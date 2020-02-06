c25d9b8ed2c934c3d34bd0448ab8c5d2af4faff7
Valentina <valena.92@gmail.com>
2020-02-06 02:14:42 +0000

    ajuste asentos fecha

--- hour.php
+++ hour.php
@@ -1,8 +1,7 @@
 <?php
-
 setlocale(LC_ALL, "es_CO");
 
 date_default_timezone_set('America/Bogota');
 
-echo '<span style="color:#2e89b6;font-size:95%;font-family: Varela Round, sans-serif;font-weight: 700; ">' .  strftime("%A %d/%b/%Y %R", mktime()) . '</span>';
-
+$date = strftime("%A %d/%b/%Y %R", mktime());
+echo '<span style="color:#2e89b6;font-size:100%;font-family: Varela Round, sans-serif;font-weight: 700; ">' . utf8_encode($date) . '</span>';
